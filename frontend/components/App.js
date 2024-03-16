89deb28fb771af6685d4ea0cbe3bc8cf3cb89004
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-03-15 18:23:27 -0700

    finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import * as yup from 'yup'
 import schemas from '../../shared/schemas'
-
+import txt from '../i18n/index.json'
 /*
   👉 TASK 2
 
@@ -11,6 +11,76 @@ import schemas from '../../shared/schemas'
   This is commonly done using libraries such as `react-i18next` or `react-intl`
   But today you will do it "by hand" using the JSON file inside the `i18n` folder
 */
+
+
+
+function getEntriesByKeyPrefix(obj, keyPrefix) {
+  /*
+    👉 TASK 4 part 1
+
+    Implement a function that takes as first argument an object `obj` such as this:
+
+    {
+      abc_1: "data_abc_1",
+      abc_2: "data_abc_2",
+      xyz_1: "data_xyz_1",
+      abc_3: "data_abc_3",
+    }
+
+    and takes as second argument a string `keyPrefix` such as this: "abc"
+
+    and returns an array of arrays such as this (for the arguments given in the examples above):
+
+    [
+      ["abc_1", "data_abc_1"],
+      ["abc_2", "data_abc_2"],
+      ["abc_3", "data_abc_3"],
+    ]
+
+    If the function is passed the same `obj` as above but a `keyPrefix` of "xyz" then it would return:
+
+    [
+      ["xyz_1", "data_xyz_1"],
+    ]
+
+    If the function is passed the same `obj` as above but a `keyPrefix` of "foo" then it would return the empty array.
+
+    The function looks inside the object `obj`, finds all properties whose property names begin
+    with the `keyPrefix` given (followed by an underscore), and reorganizes the information before returning it.
+    The properties that match the `keyPrefix` are returned inside an array holding key-value-pair sub-arrays.
+
+  */
+
+    let result = [];
+
+    for (let [key, value] of Object.entries(obj)) {
+      let compare = key.slice(0, 3)
+      console.log(compare)
+      console.log(keyPrefix)
+      if (compare === keyPrefix){
+        
+        result.push([`${key}`, `${value}`])
+      }
+      
+    }
+    console.log(result)
+    return result
+
+  }
+
+  let obj =  {
+    abc_1: "data_abc_1",
+    abc_2: "data_abc_2",
+    xyz_1: "data_xyz_1",
+    abc_3: "data_abc_3",
+  }
+
+  
+
+  getEntriesByKeyPrefix(obj, 'abc')
+
+
+
 
 const getInitialValues = () => ({ username: '', favLanguage: '', favFood: '', agreement: false })
 const getInitialValidation = () => ({ username: '', favLanguage: '', favFood: '', agreement: '' })
@@ -26,6 +96,8 @@ export default function App({ lang = 'en' }) {
   const [success, setSuccess] = useState()
   const [failure, setFailure] = useState()
   const [submitAllowed, setSubmitAllowed] = useState(false)
+
+  const text = txt[language]
 
   useEffect(() => {
     schemas.userSchema.isValid(values).then(setSubmitAllowed)
@@ -64,7 +136,7 @@ export default function App({ lang = 'en' }) {
   return (
     <div>
       <h2>
-        Create an Account
+        {text.TEXT_HEADING_CREATE_ACCOUNT}
         <span onClick={() => setLanguage(language === 'en' ? 'esp' : 'en')}>
           {language === 'en' ? ' 🇺🇸' : ' 🇪🇸'}
         </span>
@@ -74,33 +146,33 @@ export default function App({ lang = 'en' }) {
         {failure && <h4 className="error">{failure}</h4>}
 
         <div className="inputGroup">
-          <label htmlFor="username">Username:</label>
-          <input id="username" name="username" onChange={onChange} value={values.username} type="text" placeholder="Type username" />
+          <label htmlFor="username">{text.LABEL_USERNAME}</label>
+          <input id="username" name="username" onChange={onChange} value={values.username} type="text" placeholder={text.PLACEHOLDER_USERNAME} />
           {errors.username && <div className="validation">{errors.username}</div>}
         </div>
 
         <div className="inputGroup">
           <fieldset>
-            <legend>Favorite language:</legend>
+            <legend>{text.TEXT_FAV_LANG}</legend>
             <label>
               <input onChange={onChange} type="radio" name="favLanguage" value="javascript" checked={values.favLanguage == 'javascript'} />
-              JavaScript
+              {text.TEXT_FAV_LANG_JS}
             </label>
             <label>
               <input onChange={onChange} type="radio" name="favLanguage" value="rust" checked={values.favLanguage == 'rust'} />
-              Rust
+              {text.TEXT_FAV_LANG_RUST}
             </label>
           </fieldset>
           {errors.favLanguage && <div className="validation">{errors.favLanguage}</div>}
         </div>
 
         <div className="inputGroup">
-          <label htmlFor="favFood">Favorite food:</label>
+          <label htmlFor="favFood">{text.LABEL_FAV_FOOD}</label>
           <select id="favFood" name="favFood" value={values.favFood} onChange={onChange}>
-            <option value="">-- Select favorite food --</option>
-            <option value="pizza">Pizza</option>
-            <option value="spaghetti">Spaghetti</option>
-            <option value="broccoli">Broccoli</option>
+            <option value="">{text.TEXT_OPT_FAV_FOOD_1}</option>
+            <option value="pizza">{text.TEXT_OPT_FAV_FOOD_2}</option>
+            <option value="spaghetti">{text.TEXT_OPT_FAV_FOOD_3}</option>
+            <option value="broccoli">{text.TEXT_OPT_FAV_FOOD_4}</option>
           </select>
           {errors.favFood && <div className="validation">{errors.favFood}</div>}
         </div>
@@ -108,13 +180,13 @@ export default function App({ lang = 'en' }) {
         <div className="inputGroup">
           <label>
             <input id="agreement" type="checkbox" name="agreement" checked={values.agreement} onChange={onChange} />
-            Agree to our terms
+            {text.LABEL_ACCEPT_TERMS}
           </label>
           {errors.agreement && <div className="validation">{errors.agreement}</div>}
         </div>
 
         <div>
-          <input type="submit" disabled={!submitAllowed} value="Send" />
+          <input type="submit" disabled={!submitAllowed} value={text.TEXT_SUBMIT} />
         </div>
       </form>
     </div>
